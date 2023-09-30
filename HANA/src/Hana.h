0aba76c072c6417520599fff794addf348935273
SimonHadfield <simonguyhadfield@gmail.com>
2023-09-30 01:16:40 +0100

    added logging system

--- HANA/src/Hana.h
+++ HANA/src/Hana.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include "Hana/Application.h"
+#include "Hana/Log.h"
 
 // -- Entry Point
 #include "Hana/EntryPoint.h"
--- HANA/src/Hana/EntryPoint.h
+++ HANA/src/Hana/EntryPoint.h
@@ -6,6 +6,10 @@ extern Hana::Application* Hana::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hana::Log::Init();
+	Hana::Log::GetCoreLogger()->warn("Initialized Log");
+	Hana::Log::GetClientLogger()->info("Hello");
+
 	printf("Hana Engine\n");
 	auto app = Hana::CreateApplication();
 	app->Run();
